15cc13c537f981b1d55d8fc669e78184398273ea
Luigi Balestrucci <luigi@spicysparks.com>
2025-02-21 18:38:05 +0100

    Add app.ts for testing object in TS

--- app.js
+++ app.js
@@ -1,16 +1,5 @@
-function add(n1, n2, showResult, phrase) {
-    // if (typeof n1 !== 'number' || typeof n2 !== 'number'){
-    //     throw new Error('Incorrect input!');
-    // }
-    var results = n1 + n2;
-    if (showResult) {
-        console.log(phrase, results);
-    }
-    else {
-        return results;
-    }
-}
-var number1 = 5;
-var number2 = 2.8;
-var printResult = true;
-var result = add(number1, number2, printResult, "Result is: ");
+var person = {
+    name: 'Luigi',
+    age: 24
+};
+console.log(person);
--- app.ts
+++ app.ts
@@ -1,16 +1,6 @@
-function add(n1: number, n2: number, showResult: boolean, phrase: string) {
-    // if (typeof n1 !== 'number' || typeof n2 !== 'number'){
-    //     throw new Error('Incorrect input!');
-    // }
-    const results = n1 + n2;
-    if (showResult) {
-        console.log(phrase, results);
-    } else {
-        return results;
-    }
+const person = {
+    name: 'Luigi',
+    age: 24
 }
 
-const number1 = 5;
-const number2 = 2.8;
-const printResult = true;
-const result = add(number1, number2, printResult, "Result is: ");
+console.log(person);
