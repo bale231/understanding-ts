98b853ab204c70af6b5a906158f24e64548d799f
Luigi Balestrucci <luigi@spicysparks.com>
2025-02-21 17:29:32 +0100

    Practice width typeof var

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log("Your code goeas here!!");
+function add(n1, n2, showResult, phrase) {
+    // if (typeof n1 !== 'number' || typeof n2 !== 'number'){
+    //     throw new Error('Incorrect input!');
+    // }
+    var results = n1 + n2;
+    if (showResult) {
+        console.log(phrase, results);
+    }
+    else {
+        return results;
+    }
+}
+var number1 = 5;
+var number2 = 2.8;
+var printResult = true;
+var result = add(number1, number2, printResult, "Result is: ");
