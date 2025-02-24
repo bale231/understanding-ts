5248cadce7bf7c69652d4f7ed17a802aaaa348a6
Luigi Balestrucci <luigi@spicysparks.com>
2025-02-24 10:32:35 +0100

    Add new file app.ts, enums file

--- app.ts
+++
@@ -1,6 +0,0 @@
-const person = {
-    name: 'Luigi',
-    age: 24
-}
-
-console.log(person);
--- objs-arrays-enums.ts
+++ objs-arrays-enums.ts
@@ -1,19 +1,21 @@
 // const ADMIN = 0;
 // const READ_ONLY_USER = 1;
 // const AUTHOR = 2;
-var Role;
-(function (Role) {
-    Role[Role["ADMIN"] = 0] = "ADMIN";
-    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
-    Role[Role["AUTHOR"] = 2] = "AUTHOR";
-})(Role || (Role = {}));
-var person = {
+
+enum Role { ADMIN, READ_ONLY, AUTHOR }
+
+
+const person = {
     name: 'Luigi',
     age: 24,
     hobbies: ['Sports', 'Cooking'],
     role: Role.ADMIN
 };
+
+
 if (person.role === Role.AUTHOR) {
     console.log('is author');
 }
+
+
 console.log(person);
