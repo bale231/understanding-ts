3342b9134bff0fcb6383a711bb6cc30a406ca033
Luigi Balestrucci <luigi@spicysparks.com>
2025-03-03 14:37:15 +0100

    Add function file lessons

---
+++ function_as_types.ts
@@ -0,0 +1,17 @@
+// function add(n1: number, n2: number) {
+//   return n1 + n2;
+// }
+
+// // questa funzione restituisce "void" perchè si tratta di una funzione che si esegue con successo sempre.
+// function printResult(num: number) {
+//   console.log("Result:", num);
+// }
+
+// let combineValues: (a: number, b: number) => number;
+
+// combineValues = add;
+// // combineValues = 5;
+
+// console.log(combineValues(8, 8));
+
+// // printResult(add(5, 12));
