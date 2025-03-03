a9ed75d5347b8e305ed493463867a0cb2cc7eb87
Luigi Balestrucci <luigi@spicysparks.com>
2025-03-03 11:04:48 +0100

    Add function lessons in app.ts

---
+++ app.ts
@@ -0,0 +1,10 @@
+function add(n1: number, n2: number) {
+  return n1 + n2;
+}
+
+// questa funzione restituisce "void" perchè si tratta di una funzione che si esegue con successo sempre.
+function printResult(num: number) {
+  console.log("Result:", num);
+}
+
+printResult(add(5, 12));
