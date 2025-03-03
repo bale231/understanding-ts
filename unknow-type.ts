3ee4dcd7e5002a0ac9455703bd81f5e4aeb8b170
Luigi Balestrucci <luigi@spicysparks.com>
2025-03-03 14:48:52 +0100

    Add unknow-type file

---
+++ unknow-type.ts
@@ -0,0 +1,11 @@
+// Con unknow non si può assegnare la variabile userName al valore di userInput, cosa che, invece, si può fare con any
+let userInput: unknown;
+// let userInput: any;
+let userName: string;
+
+userInput = 5;
+userInput = "Max";
+if (typeof userInput === "string") {
+    userName = userInput;
+    console.log(userName);
+}
