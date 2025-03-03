ab27beb35b372aad81850425de83a3745971c534
Luigi Balestrucci <luigi@spicysparks.com>
2025-03-03 10:34:22 +0100

    Add union aliases

--- literal-types.ts
+++ literal-types.ts
@@ -1,31 +1,31 @@
-function combine(
-  input1: number | string,
-  input2: number | string,
-  resultConversion: "as-number" | "as-string"
-) {
-  let result;
+// function combine(
+//   input1: number | string,
+//   input2: number | string,
+//   resultConversion: "as-number" | "as-string"
+// ) {
+//   let result;
 
-  if (
-    (typeof input1 === "number" && typeof input2 === "number") ||
-    resultConversion === "as-number"
-  ) {
-    result = +input1 + +input2;
-  } else {
-    result = input1.toString() + input2.toString();
-  }
+//   if (
+//     (typeof input1 === "number" && typeof input2 === "number") ||
+//     resultConversion === "as-number"
+//   ) {
+//     result = +input1 + +input2;
+//   } else {
+//     result = input1.toString() + input2.toString();
+//   }
 
-  return result;
-  // if (resultConversion === 'as-number') {
-  //     return +result;
-  // } else {
-  //     return result.toString();
-  // }
-}
+//   return result;
+//   // if (resultConversion === 'as-number') {
+//   //     return +result;
+//   // } else {
+//   //     return result.toString();
+//   // }
+// }
 
-const combinedAges = combine(30, 26, "as-number");
-const combineNames = combine("Max", "Anna", "as-string");
-const combinedStringAges = combine("30", "26", "as-number");
+// const combinedAges = combine(30, 26, "as-number");
+// const combineNames = combine("Max", "Anna", "as-string");
+// const combinedStringAges = combine("30", "26", "as-number");
 
-console.log(combineNames);
-console.log(combinedAges);
-console.log(combinedStringAges);
+// console.log(combineNames);
+// console.log(combinedAges);
+// console.log(combinedStringAges);
